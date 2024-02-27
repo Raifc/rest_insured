cbf90f1f7c3915abfa0314d8f6da9c4fbd86415a
Raifc <raif.si@hotmail.com>
2024-02-27 18:57:38 -0300

    Updates policy worker and removes sneakers from docker-compose

--- app/workers/policy_worker.rb
+++ app/workers/policy_worker.rb
@@ -1,31 +1,46 @@
+# frozen_string_literal: true
+
 require 'sneakers'
 require_relative '../../config/environment'
-require_relative '../models/policy.rb'
+require_relative '../models/policy'
 
 class PolicyWorker
   include Sneakers::Worker
   from_queue 'policy-created', durable: true
 
   def work(msg)
-    begin
-      puts '======= Creating Policy ======='
-      payload = JSON.parse msg
-      vehicle = Vehicle.create!(payload['vehicle'])
-      customer = Customer.create!(payload['customer'])
+    puts '======= Creating Policy ======='
+    payload = JSON.parse(msg)
+    create_policy(payload)
+  rescue StandardError => e
+    puts "Error creating policy #{e.inspect}"
+    reject!
+  end
+
+  private
+
+  def create_policy(payload)
+    ActiveRecord::Base.connection_pool.with_connection do
+      vehicle = create_vehicle(payload['vehicle'])
+      customer = create_customer(payload['customer'])
 
       policy = Policy.create!(
         effective_from: Date.parse(payload['effective_from']),
         effective_until: Date.parse(payload['effective_until']),
-        customer: customer,
-        vehicle: vehicle,
+        customer:,
+        vehicle:
       )
 
       puts "Policy created: #{policy.inspect}"
-
       ack!
-    rescue StandardError => e
-      puts "Error creating policy #{e.inspect}"
-      reject!
     end
+  end
+
+  def create_vehicle(vehicle_params)
+    Vehicle.create!(vehicle_params)
+  end
+
+  def create_customer(customer_params)
+    Customer.create!(customer_params)
   end
 end
