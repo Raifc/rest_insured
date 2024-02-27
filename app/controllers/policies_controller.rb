0424c93b2f4a206070aa2a5e69fa9efca84a9bbd
Raifc <raif.si@hotmail.com>
2024-02-27 16:54:46 -0300

    Adjusts serializer

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -4,21 +4,10 @@ class PoliciesController < ApplicationController
   before_action :set_policy, only: [:show]
 
   def show
-    render json: {
-      policy_id: @policy.id,
-      effective_from: @policy.effective_from,
-      effective_until: @policy.effective_until,
-      insured_person: {
-        name: @policy.insured_person.name,
-        cpf: @policy.insured_person.document_number
-      },
-      vehicle:{
-        make: @policy.vehicle.make,
-        model: @policy.vehicle.model,
-        year: @policy.vehicle.year,
-        plate: @policy.vehicle.plate
-      }
-    }
+    render(
+      json: @policy,
+      serializer: PolicySerializer
+    )
   end
 
   private
