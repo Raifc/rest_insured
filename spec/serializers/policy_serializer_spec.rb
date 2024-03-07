96dc64a88972def3f1d83bcf3fb421f59df5c880
Raifc <raif.si@hotmail.com>
2024-03-07 19:44:33 -0300

    minor fix on test

--- spec/serializers/policy_serializer_spec.rb
+++ spec/serializers/policy_serializer_spec.rb
@@ -1,13 +1,11 @@
-# spec/serializers/policy_serializer_spec.rb
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe PolicySerializer do
   let(:customer) { Customer.create(name: 'John Doe', document_number: '123456789') }
   let(:vehicle) { Vehicle.create(make: 'Toyota', model: 'Corolla', year: 2020, plate: 'ABC1234') }
-  let(:policy) do
-    Policy.create(customer:, vehicle:, effective_from: '2022-03-02', effective_until: '2024-03-02')
-  end
+  let(:policy) { Policy.create(customer:, vehicle:, effective_from: '2022-03-02', effective_until: '2024-03-02') }
   let(:serializer) { described_class.new(policy) }
   let(:serialization) { ActiveModelSerializers::Adapter.create(serializer).as_json }
 
@@ -28,7 +26,7 @@ RSpec.describe PolicySerializer do
           plate: 'ABC1234'
         }
       }
-      
+
       expect(serializer.attributes).to eq(expected_result)
     end
   end
