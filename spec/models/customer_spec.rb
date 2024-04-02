51341338eee025245109dac1706e3b278195935b
Raifc <raif.si@hotmail.com>
2024-04-01 21:28:33 -0300

    Adds context to tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,19 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  it 'is valid with valid attributes' do
-    customer = Customer.create(name: 'John Doe', document_number: '1234567890')
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      customer = Customer.create(name: 'John Doe', document_number: '1234567890')
 
-    expect(customer.valid?).to be_truthy
-  end
+      expect(customer.valid?).to be_truthy
+    end
 
-  it 'is invalid without required attributes' do
-    customer = Customer.create
+    it 'is invalid without required attributes' do
+      customer = Customer.create
 
-    expect(customer).not_to be_valid
-    expect(customer.errors.full_messages).to include(
-      "Name can't be blank",
-      "Document number can't be blank"
-    )
+      expect(customer).not_to be_valid
+      expect(customer.errors.full_messages).to include(
+        "Name can't be blank",
+        "Document number can't be blank"
+      )
+    end
   end
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -3,21 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  it 'is valid with valid attributes' do
-    vehicle = Vehicle.create(plate: 'ABC123', make: 'Toyota', model: 'Camry', year: '2022')
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      vehicle = Vehicle.create(plate: 'ABC123', make: 'Toyota', model: 'Camry', year: '2022')
 
-    expect(vehicle.valid?).to be_truthy
-  end
+      expect(vehicle.valid?).to be_truthy
+    end
 
-  it 'is invalid without required attributes' do
-    vehicle = Vehicle.create
+    it 'is invalid without required attributes' do
+      vehicle = Vehicle.create
 
-    expect(vehicle).not_to be_valid
-    expect(vehicle.errors.full_messages).to include(
-      "Plate can't be blank",
-      "Make can't be blank",
-      "Model can't be blank",
-      "Year can't be blank"
-    )
+      expect(vehicle).not_to be_valid
+      expect(vehicle.errors.full_messages).to include(
+        "Plate can't be blank",
+        "Make can't be blank",
+        "Model can't be blank",
+        "Year can't be blank"
+      )
+    end
   end
 end
